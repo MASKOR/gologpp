66851af5773c32afd6177e1bc15a0e40e5b4be0f
Victor Mataré <matare@fh-aachen.de>
2018-02-19 13:29:12 +0100

    remove dead code

--- src/backends/readylog/Action.cpp
+++ src/backends/readylog/Action.cpp
@@ -8,36 +8,6 @@
 #include <utility>
 
 namespace gologpp {
-namespace readylog {
-
-/*Action::Action(generic::Action &&action)
-: Translatable<generic::Action, tuple<EC_word, EC_word, EC_word>>(std::move(action))
-{}
-
-
-void Action::init(tuple<EC_word, EC_word, EC_word> &cache)
-{
-	EC_word *args = new EC_word[arity()];
-	EC_word prim_action = ::term(EC_functor("prim_action", 1), ::term(
-	                  EC_functor(name().c_str(), arity()), args));
-
-	EC_word poss = dynamic_cast<const Translatable<generic::BooleanExpression, EC_word> &>(precondition()).translate();
-	EC_word causes_val = dynamic_cast<const gologpp::readylog::EffectAxiom &>(effect()).translate();
-
-	cache = std::tie(prim_action, poss, causes_val);
-}*/
-
-} // namespace readylog
-
-
-/*void generic::Action::init_impl(unique_ptr<Implementation<Action>> &impl)
-{
-	if (!impl) {
-		impl.reset(new Implementation<generic::Action>(*this));
-		precondition_->init();
-	}
-}*/
-
 
 Implementation<generic::Action>::Implementation(const generic::Action &a)
 : action_(a)
