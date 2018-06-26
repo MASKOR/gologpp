af1ca0eb7354aa6b317efd905ea6497689a43535
Victor Mataré <matare@fh-aachen.de>
2018-06-26 18:35:28 +0200

    impl/readylog: Reference simply compiles into target term

--- src/impl/readylog/reference.h
+++ src/impl/readylog/reference.h
@@ -20,23 +20,15 @@ template<class GologT> class Reference;
 template<class GologT>
 class Implementation<Reference<GologT>> : public ReadylogImplementation {
 public:
-	Implementation(const Reference<GologT> &ref)
+	Implementation(Reference<GologT> &ref)
 	: reference_(ref)
 	{}
 
-
 	EC_word term() override
-	{
-		EC_word *args = new EC_word[reference_.args().size()];
-		arity_t i = 0;
-		for (const unique_ptr<Expression> &expr : reference_.args())
-			args[i++] = dynamic_cast<ReadylogImplementation &>(expr->implementation()).term();
-		return ::term(EC_functor(reference_.name().c_str(), reference_.arity()), args);
-	}
-
+	{ return reference_.target().implementation().term(); }
 
 private:
-	const Reference<GologT> &reference_;
+	Reference<GologT> &reference_;
 };
 
 
