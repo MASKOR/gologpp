0fb91282d9283d581630504a98b8ea90e78b0f21
Victor Mataré <matare@fh-aachen.de>
2018-10-13 03:54:58 +0200

    model: add missing auto-gen for virtual make_semantics

--- src/model/semantics.h
+++ src/model/semantics.h
@@ -4,6 +4,8 @@
 #include "gologpp.h"
 #include <memory>
 
+#include <boost/preprocessor/seq/for_each.hpp>
+
 namespace gologpp {
 
 
@@ -17,69 +19,14 @@ public:
 };
 
 
+#define GOLOGPP_DECLARE_ABSTRACT_MAKE_SEMANTICS(r, data, T) \
+	virtual unique_ptr<AbstractSemantics> make_semantics(T &) = 0;
+
 class SemanticsFactory {
 public:
 	virtual ~SemanticsFactory() = default;
 
-	virtual unique_ptr<AbstractSemantics> make_semantics(Action &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(ExogAction &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Transition &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(ExogTransition &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Reference<Action> &) = 0;
-
-	virtual unique_ptr<AbstractSemantics> make_semantics(EffectAxiom<BooleanExpression> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(EffectAxiom<NumericExpression> &) = 0;
-
-	virtual unique_ptr<AbstractSemantics> make_semantics(InitialValue<BooleanExpression> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(InitialValue<NumericExpression> &) = 0;
-
-	virtual unique_ptr<AbstractSemantics> make_semantics(Fluent<BooleanExpression> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Fluent<NumericExpression> &) = 0;
-
-	virtual unique_ptr<AbstractSemantics> make_semantics(Reference<Fluent<BooleanExpression>> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Reference<Fluent<NumericExpression>> &) = 0;
-
-	virtual unique_ptr<AbstractSemantics> make_semantics(Scope &) = 0;
-
-	virtual unique_ptr<AbstractSemantics> make_semantics(Constant<BooleanExpression> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Constant<NumericExpression> &) = 0;
-
-	virtual unique_ptr<AbstractSemantics> make_semantics(Variable<BooleanExpression> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Variable<NumericExpression> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Reference<Variable<BooleanExpression>> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Reference<Variable<NumericExpression>> &) = 0;
-
-	virtual unique_ptr<AbstractSemantics> make_semantics(ArithmeticOperation &) = 0;
-
-	virtual unique_ptr<AbstractSemantics> make_semantics(Negation &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Comparison &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(BooleanOperation &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Quantification &) = 0;
-
-	virtual unique_ptr<AbstractSemantics> make_semantics(Block &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Choose &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Conditional &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Assignment<BooleanFluent> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Assignment<NumericFluent> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Assignment<BooleanVariable> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Assignment<NumericVariable> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Pick<BooleanExpression> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Pick<NumericExpression> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Search &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Solve &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Test &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(While &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Return<BooleanExpression> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Return<NumericExpression> &) = 0;
-
-	virtual unique_ptr<AbstractSemantics> make_semantics(Function<BooleanExpression> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Function<NumericExpression> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Function<Statement> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Reference<Function<BooleanExpression>> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Reference<Function<NumericExpression>> &) = 0;
-	virtual unique_ptr<AbstractSemantics> make_semantics(Reference<Function<Statement>> &) = 0;
-
-	virtual unique_ptr<AbstractSemantics> make_semantics(History &) = 0;
+	BOOST_PP_SEQ_FOR_EACH(GOLOGPP_DECLARE_ABSTRACT_MAKE_SEMANTICS, (), GOLOGPP_SEMANTIC_TYPES)
 };
 
 
