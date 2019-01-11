ec6e0cec4525771f527e694fab900768bc915760
Victor Mataré <matare@fh-aachen.de>
2019-01-11 16:20:48 +0100

    readylog: define durative action semantics
    Durative actions are asserted with durative_action/1, durative_poss/2
    and durative_causes_val/4. The semantics defined here then derives
    appropriate start/stop/finish/fail primitive actions, where the
    precondition only applies to the start action and the effects only apply
    to the finish action. The finish and fail actions become possible only
    after the platform backend has notified golog++ via an appropriate
    exogenous action. Stopping a currenty running action is always possible.

--- src/semantics/readylog/boilerplate.pl
+++ src/semantics/readylog/boilerplate.pl
@@ -1,15 +1,104 @@
+:- external(exog_fluent_getValue/3, p_exog_fluent_getValue).
+:- dynamic managed_term/2.
+
 function(strcat(X, Y), R, concat_atoms(X, Y, R)).
+
+
+/********************************
+ * Durative Action semantics
+ * ******************************/
+
+exog_prim_fluent(now).
+
+prim_fluent(state(A)) :- durative_action(A).
+initial_val(state(A), idle) :- durative_action(A).
+reserved_fluent(state(_A)).
+
+exog_action(exog_state_change(A, _T, State)) :- durative_action(A).
+poss(exog_state_change(A, _T, State), true) :- durative_action(A).
+causes_val(exog_state_change(A, _T, State), state(A), State, true) :- durative_action(A).
+
+
+% start action
+% ============
+prim_action(start(A, _T)) :- durative_action(A).
+
+% Precondition only applies to starting a durative action
+poss(start(A, T),
+	and([
+		not(state(A) = running)
+		, now >= T
+		, C
+	])
+) :-
+	durative_action(A)
+	, durative_poss(A, C)
+.
+causes_val(start(A, _T), state(A), running, true) :- durative_action(A).
+
+
+% finish action
+% ============
+prim_action(finish(A, _T)) :- durative_action(A).
+poss(finish(A, T),
+	and(
+		lif(online = true
+			% Online: Wait for real action to complete
+			, state(A) = final
+			% Offline: Final immediately
+			, true
+		)
+		, now >= T
+	)
+) :- durative_action(A).
+causes_val(finish(A, _T), state(A), final, true) :- durative_action(A).
+
+% Only the finish action applies a durative action's effects:
+causes_val(finish(A, T), F, V, C) :- durative_causes_val(A, F, V, C).
+
+
+% stop action
+% ===========
+prim_action(stop(A, _T)) :- durative_action(A).
+poss(stop(A, T),
+	and(
+		lif(online = true
+			, state(A) = running
+			, true
+		)
+		, now >= T
+	)
+) :- durative_action(A).
+causes_val(stop(A, _T), state(A), preempted, true) :- durative_action(A).
+
+
+% fail action
+% =============
+prim_action(fail(A, _T)) :- durative_action(A).
+poss(fail(A, T),
+	and([
+		online = true
+		, state(A) = failed
+		, now >= T
+	])
+) :- durative_action(A).
+causes_val(fail(A, _T), state(A), failed, true) :- durative_action(A).
+
+
+
+/********************************/
+
 
 ssa( online, false, [clipOnline|_69485] ) :- !, true.
 ssa( online, true, [setOnline|_69524] ) :- !, true.
-ssa( start, T, [setTime(T)|_69563] ) :- !, true.
-ssa( start, T, [ccUpdate(_69409, T)|_69585] ) :- !, true.
+ssa( sit_start_time, T, [setTime(T)|_69563] ) :- !, true.
+ssa( sit_start_time, T, [ccUpdate(_69409, T)|_69585] ) :- !, true.
 ssa( Fluent, Value, [set(Fluent, Value)|_69607] ) :- !, true.
 
 ssa( eval_exog_functions, _69895, [set(eval_exog_functions, _69895)|_] ) :- !.
 ssa( eval_registers, _69906, [set(eval_registers, _69906)|_] ) :- !.
 ssa( online, _69917, [set(online, _69917)|_] ) :- !.
-ssa( start, _69928, [set(start, _69928)|_] ) :- !.
+ssa( sit_start_time, _69928, [set(sit_start_time, _69928)|_] ) :- !.
 ssa( useAbstraction, _69939, [set(useAbstraction, _69939)|_] ) :- !.
 ssa( bel(_69762), _69950, [set(bel(_69762), _69950)|_] ) :- !.
 ssa( ltp(_69766), _69961, [set(ltp(_69766), _69961)|_] ) :- !.
@@ -19,7 +108,7 @@ ssa( pll(_69743, _69744, _69745, _69746), _70159, [set(pll(_69743, _69744, _6974
 ssa( eval_exog_functions, _70170, [_70174|_70175] ) :- !, has_val(eval_exog_functions, _70170, _70175).
 ssa( eval_registers, _70190, [_70194|_70195] ) :- !, has_val(eval_registers, _70190, _70195).
 ssa( online, _70210, [_70214|_70215] ) :- !, has_val(online, _70210, _70215).
-ssa( start, _70230, [_70234|_70235] ) :- !, has_val(start, _70230, _70235).
+ssa( sit_start_time, _70230, [_70234|_70235] ) :- !, has_val(sit_start_time, _70230, _70235).
 ssa( useAbstraction, _70250, [_70254|_70255] ) :- !, has_val(useAbstraction, _70250, _70255).
 ssa( bel(_69762), _70270, [_70274|_70275] ) :- !, has_val(bel(_69762), _70270, _70275).
 ssa( ltp(_69766), _70290, [_70294|_70295] ) :- !, has_val(ltp(_69766), _70290, _70295).
