321658c4abf05a718eeb9f4e8a50cd320179afb7
Victor Mataré <matare@fh-aachen.de>
2019-03-12 17:44:53 +0100

    parser: fix Domain parser on Ubuntu 16.04
    another one with the omitted empty brackets when a rule has no args

--- src/parser/domain.h
+++ src/parser/domain.h
@@ -16,7 +16,7 @@ struct DomainExpressionParser : grammar<Domain<ExprT>(Scope &)> {
 	rule<Domain<ExprT>(Scope &)> domain_expr;
 	rule<Domain<ExprT>(Scope &)> unary_domain_expr;
 	rule<Domain<ExprT>(Scope &)> binary_domain_expr;
-	rule<DomainOperator> domain_operator;
+	rule<DomainOperator()> domain_operator;
 	ConstantParser<ExprT, true> constant;
 };
 
