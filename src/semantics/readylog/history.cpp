5d3fb8e1e2cf4750764abe8fffa0941376c6ee0d
Victor Mataré <matare@fh-aachen.de>
2019-04-02 16:29:17 +0200

    readylog: ignore unknown transitions
    Turns out there's occasionally some other stuff going that isn't of
    interest at the golog++ level, like toss(*), but also set/2 and possibly
    other things. In general it seems reasonable to just silently ignore
    unknown actions in the history.

--- src/semantics/readylog/history.cpp
+++ src/semantics/readylog/history.cpp
@@ -91,13 +91,10 @@ shared_ptr<Transition> Semantics<History>::get_last_transition()
 	EC_word head = get_history_head();
 	string headname = get_head_name(head);
 
-	if (headname == "set" && head.arity() == 2)
-		return nullptr;
-
 	auto state_it = name2state.find(headname);
 
 	if (state_it == name2state.end())
-		throw EngineError("Unhandled transition: " + headname);
+		return nullptr;
 
 	if (head.arity() != 2)
 		throw EngineError("Transition arity must be 2: " + headname);
