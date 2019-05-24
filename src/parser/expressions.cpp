25f1acede211ba607f21a9c4082d77538e7f3114
Victor Mataré <matare@fh-aachen.de>
2019-05-25 00:12:19 +0200

    parser: implement lazy typed_expression()
    runtime rule selection is now working, should improve complexity
    considerably.

--- src/parser/expressions.cpp
+++ src/parser/expressions.cpp
@@ -41,33 +41,15 @@ void define_expression_rules()
 	static SymbolicExpressionParser symbolic_expression_;
 	static CompoundExpressionParser compound_expression_;
 
-	boolean_expression = { boolean_expression_(_r1) };
-	numeric_expression = { numeric_expression_(_r1) };
-	string_expression = { string_expression_(_r1) };
-	symbolic_expression = { symbolic_expression_(_r1) };
-	compound_expression = { compound_expression_(_r1) };
+	boolean_expression = { boolean_expression_(_r1), "boolean_expression" };
+	numeric_expression = { numeric_expression_(_r1), "numeric_expression" };
+	string_expression = { string_expression_(_r1), "string_expression" };
+	symbolic_expression = { symbolic_expression_(_r1), "symbolic_expression" };
+	compound_expression = { compound_expression_(_r1), "compound_expression" };
+
+	GOLOGPP_DEBUG_NODES((boolean_expression)(numeric_expression)
+		(string_expression)(symbolic_expression)(compound_expression))
 }
-
-
-/*
-static rule<Expression *(Scope &)> &get_expression_parser(Typename t)
-{
-	static std::unordered_map <
-		Typename,
-		std::reference_wrapper<rule<Expression *(Scope &)>>
-	> expr_parser_map {
-		{ Number::static_name(), numeric_expression },
-		{ Bool::static_name(), boolean_expression },
-		{ String::static_name(), string_expression },
-		{ Symbol::static_name(), symbolic_expression },
-	};
-
-	auto it = expr_parser_map.find(t);
-	if (it == expr_parser_map.end())
-		return compound_expression;
-	else
-		return it->second;
-}//*/
 
 
 rule<Expression *(Scope &)> &value_expression()
@@ -85,32 +67,65 @@ rule<Expression *(Scope &)> &value_expression()
 }
 
 
-/*rule<Expression *(Scope &, Typename)> &typed_expression()
+
+static rule<Expression *(Scope *)> &get_expression_parser(Typename t)
 {
-	static rule<Expression *(Scope &, rule<Expression *(Scope &)>)> lazyinvoke {
-		lazy(phoenix::bind(_r2, ref(_r1)))
+	static BooleanExpressionParser boolean_expression_;
+	static NumericExpressionParser numeric_expression_;
+	static StringExpressionParser string_expression_;
+	static SymbolicExpressionParser symbolic_expression_;
+	static CompoundExpressionParser compound_expression_;
+
+	static std::unordered_map <
+		Typename,
+		rule<Expression *(Scope *)>
+	> expr_parser_map {
+		{ NumberType::name(), { numeric_expression_(*_r1), "numeric_expression" } },
+		{ BoolType::name(), { boolean_expression_(*_r1), "boolean_expression" } },
+		{ StringType::name(), { string_expression_(*_r1), "string_expression" } },
+		{ SymbolType::name(), { symbolic_expression_(*_r1), "symbolic_expression" } },
 	};
+
+	GOLOGPP_DEBUG_NODE(expr_parser_map[NumberType::name()])
+	GOLOGPP_DEBUG_NODE(expr_parser_map[BoolType::name()])
+	GOLOGPP_DEBUG_NODE(expr_parser_map[StringType::name()])
+	GOLOGPP_DEBUG_NODE(expr_parser_map[SymbolType::name()])
+
+	static rule<Expression *(Scope *)> compound_expression {
+		compound_expression_(*_r1)
+		, "compound_expression"
+	};
+
+	auto it = expr_parser_map.find(t);
+	if (it == expr_parser_map.end())
+		return compound_expression;
+	else
+		return it->second;
+}
+
+
+rule<Expression *(Scope &, Typename)> &typed_expression()
+{
+	/// This crazy hack allows us to pass arguments to lazy parsers.
+	/// cf. @a https://stackoverflow.com/questions/56221006/boostspirit-lazy-parser-with-arguments
+	/// Note that the argument to the lazily returned parser must be copyable, so the
+	/// Scope& has to become Scope* here.
+	static rule<
+		Expression *(
+			Scope &,
+			rule<Expression *(Scope *)>
+		)
+	> lazyinvoke {
+		lazy(phoenix::bind(_r2, &_r1))
+	};
+
 	static rule<Expression *(Scope &, Typename)> rv {
 		lazyinvoke(_r1, phoenix::bind(&get_expression_parser, _r2))
 		, "typed_expression"
 	};
 	GOLOGPP_DEBUG_NODE(rv)
 	return rv;
-}//*/
-
-
-rule<Expression *(Scope &, Typename)> &typed_expression()
-{
-	static rule<Expression *(Scope &, Typename)> rv {
-		value_expression()(_r1) [
-			_pass = phoenix::bind(&Expression::type_name, _1) == _r2
-		]
-	};
-	GOLOGPP_DEBUG_NODE(rv)
-	return rv;
-}//*/
-
-
+}
 
 
 }
