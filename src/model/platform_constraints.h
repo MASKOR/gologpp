784529b59141d75f1cb1885128322a82e6738eee
Tarik Viehmann <tarik.viehmann@rwth-aachen.de>
2020-03-06 16:31:22 +0100

    model: add prototype datastructure for platform models
    A very crude representation for plaform models that form a subset of
    timed automata. Mainly as a placeholder until a proper object model is
    derived.

--- src/model/platform_constraints.h
+++ src/model/platform_constraints.h
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with golog++.  If not, see <https://www.gnu.org/licenses/>.
+ * along with golog++.	If not, see <https://www.gnu.org/licenses/>.
 **************************************************************************/
 
 #ifndef PLATFORM_CONSTRAINTS_H
