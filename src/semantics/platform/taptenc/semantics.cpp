b2d8cc17357acf979671916167fb81356d935107
Victor Mataré <matare@fh-aachen.de>
2020-07-30 20:19:15 +0200

    add taptenc to cmake build

---
+++ src/semantics/platform/taptenc/semantics.cpp
@@ -0,0 +1,34 @@
+/*************************************************************************
+ * This file is part of golog++.
+ *
+ * golog++ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * golog++ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with golog++.  If not, see <https://www.gnu.org/licenses/>.
+**************************************************************************/
+
+#include <model/platform/clock_formula.h>
+#include <model/platform/component.h>
+#include <model/platform/reference.h>
+#include <model/platform/constraint.h>
+
+#include "semantics.h"
+
+
+namespace gologpp {
+namespace platform {
+
+
+BOOST_PP_SEQ_FOR_EACH(GOLOGPP_DEFINE_MAKE_SEMANTICS, TaptencSemanticsFactory, GOLOGPP_PLATFORM_ELEMENTS)
+
+
+} // namespace platform
+} // namespace gologpp
---
+++ src/semantics/platform/taptenc/semantics.h
@@ -0,0 +1,41 @@
+/*************************************************************************
+ * This file is part of golog++.
+ *
+ * golog++ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * golog++ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with golog++.  If not, see <https://www.gnu.org/licenses/>.
+**************************************************************************/
+
+#pragma once
+
+#include <model/platform/semantics.h>
+
+
+namespace gologpp {
+namespace platform {
+
+
+template<class PlatformT>
+class Semantics : public GeneralSemantics<PlatformT> {
+public:
+	using GeneralSemantics<PlatformT>::GeneralSemantics;
+};
+
+
+class TaptencSemanticsFactory : public SemanticsFactory {
+public:
+	BOOST_PP_SEQ_FOR_EACH(GOLOGPP_DECL_MAKE_SEMANTICS_OVERRIDE, (), GOLOGPP_PLATFORM_ELEMENTS)
+};
+
+
+} // namespace platform
+} // namespace gologpp
